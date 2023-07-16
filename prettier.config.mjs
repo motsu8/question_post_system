2e6af9bb7b23c9caf6ccb2e9db98fdf47a7b0b19
motsu8e <motsu159357@gmail.com>
2023-07-16 19:06:26 +0900

    coment delete

--- prettier.config.mjs
+++ prettier.config.mjs
@@ -1,22 +1,4 @@
-// module.exports = {
-//     printWidth: 100,
-//     tabWidth: 2,
-//     useTabs: false,
-//     semi: true,
-//     singleQuote: false,
-//     quoteProps: "as-needed",
-//     jsxSingleQuote: false,
-//     trailingComma: "es5",
-//     bracketSpacing: true,
-//     bracketSameLine: false,
-//     arrowParens: "always",
-//     proseWrap: "preserve",
-//     htmlWhitespaceSensitivity: "css",
-//     endOfLine: "lf",
-//     embeddedLanguageFormatting: "off",
-//   };
-
-  // prettier.config.js, .prettierrc.js, prettier.config.mjs, or .prettierrc.mjs
+// prettier.config.js, .prettierrc.js, prettier.config.mjs, or .prettierrc.mjs
 
 /** @type {import("prettier").Options} */
 const config = {
