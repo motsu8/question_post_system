0b85046798a7596a0dd18dcbdc5c8f14c0cf33f1
motsu8e <motsu159357@gmail.com>
2023-07-18 00:39:16 +0900

    fixed a bug that html element disappear

--- src/browser/components/DiscordOauth.tsx
+++ src/browser/components/DiscordOauth.tsx
@@ -19,7 +19,8 @@ function DiscordOauth() {
     const [accessToken, tokenType] = [fragment.get("access_token"), fragment.get("token_type")];
     if (!accessToken) {
       const login = document.getElementById("login") as HTMLElement;
-      return toggleDisplay(login);
+      toggleDisplay(login);
+      return;
     }
 
     let imgUrl;
@@ -45,7 +46,7 @@ function DiscordOauth() {
         target="_blank"
         rel="noreferrer"
         href="https://discord.com/api/oauth2/authorize?client_id=1130079596839182376&redirect_uri=http%3A%2F%2Flocalhost%3A53134&response_type=token&scope=identify%20guilds%20guilds.members.read"
-        className="text-indigo-500 hover:text-indigo-300 font-medium rounded-full text-sm px-4 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+        className="hidden text-indigo-500 hover:text-indigo-300 font-medium rounded-full text-sm px-4 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
       >
         Discordと連携する
       </a>
